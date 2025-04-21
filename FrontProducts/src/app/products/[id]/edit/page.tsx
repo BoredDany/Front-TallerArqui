71a875d38ecb57190b3fda9c6c5c0942d2f0c832
BoredDany <martinezq.d@javeriana.edu.co>
2025-04-21 01:05:16 -0500

    fixed async param

--- FrontProducts/src/app/products/[id]/edit/page.tsx
+++ FrontProducts/src/app/products/[id]/edit/page.tsx
@@ -2,19 +2,20 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { EditProductForm } from "./edit-product-form";
 import { getProduct } from "../../products.api";
 
-async function ProductsEditPage({ params }: { params: { id: string } }) {
-    const product = await getProduct(params.id);
-    return (
-        <div className="hScreen flex items-center justify-center">
-        <Card>
-            <CardHeader>
-            <CardTitle>Edit Product</CardTitle>
-            </CardHeader>
-            <CardContent>
-            <EditProductForm product = {product} />
-            </CardContent>
-        </Card>
-        </div>
-    );
+export default async function ProductsEditPage({ params }: { params: { id: string } }) {
+  const { id } = await params
+  const product = await getProduct(id); // Obtener el producto con el id
+
+  return (
+    <div className="hScreen flex items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Edit Product</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <EditProductForm product={product} />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
-export default ProductsEditPage;
